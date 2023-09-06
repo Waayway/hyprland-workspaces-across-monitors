638787cff1b8201fc2271bac5ad6852e63deafa3
Thijs van Waaij <thijs@vanwaaij.com>
2023-09-06 22:10:52 +0200

    flake now hopefully done

--- flake.nix
+++ flake.nix
@@ -5,33 +5,41 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, flake-utils, ... }: flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
-    let
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-
-
-      wam = pkgs.rustPlatform.buildRustPackage
-        {
-          pname = "workspace-across-monitors";
-          version = "0.0.1";
-          src = ./.;
-
-          cargoLock = {
-            lockFile = ./Cargo.lock;
-          };
-
-          nativeBuildInputs = [ pkgs.pkg-config ];
-          PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
         };
-        in {
-        defaultPackage = wam;
-      devShell = pkgs.mkShell {
-        buildInputs = [
-          wam
-        ];
-      };
+
+        wam = pkgs.rustPlatform.buildRustPackage
+          {
+            pname = "workspace-across-monitors";
+            version = "0.0.1";
+            src = ./.;
+
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+            };
+
+            nativeBuildInputs = [ pkgs.pkg-config ];
+            PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+          };
+      in
+      {
+        packages = rec {
+          wam = wam;
+          defaultPackage.${system} = wam;
+        };
+        apps = rec {
+          wam = flake-utils.lib.mkApp { drv = self.packages.${system}.wam; };
+          default = wam;
+        };
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            wam
+          ];
+        };
       }
-      );
-      }
+    );
+}
