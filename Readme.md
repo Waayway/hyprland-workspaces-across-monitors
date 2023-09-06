cf37757c3e712fea2a38441cd79ca7fbb54d3b09
Thijs van Waaij <thijs@vanwaaij.com>
2023-09-06 21:50:26 +0200

    flake and new application name

--- Readme.md
+++ Readme.md
@@ -1,4 +1,14 @@
 # Hyprland Workspaces across multiple monitors
 
-
 I Don't like the independant monitors so i made a smol program that didn't need it
+## How to use
+Install (i use nixos btw) so recommended is currently to use nix
+
+
+```conf
+# Instead of
+bind =  $mainMod, 1, workspace 1
+# you use
+bind = $mainMod, 1, exec, wam workspace 1
+```
+so the thing that changes is the exec and the wam
--- flake.nix
+++ flake.nix
@@ -1,24 +1,37 @@
 {
-  description = "A Nix-flake-based Shell development environment";
+  description = "Hyprland Workspaces across multiple monitors";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-
-  outputs = { self, nixpkgs }:
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils, ... }: flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
     let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit system; };
-      });
-    in
-    {
-      devShells = forEachSupportedSystem
-        ({ pkgs }: {
-          default = pkgs.mkShell {
-            shellHook = ''
-              export PATH=$PATH:$(pwd)/target/debug
-            '';
-            packages = with pkgs; [  ];
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+
+
+      wam = pkgs.rustPlatform.buildRustPackage
+        {
+          pname = "workspace-across-monitors";
+          version = "0.0.1";
+          src = ./.;
+
+          cargoLock = {
+            lockFile = ./Cargo.lock;
           };
-        });
-    };
-}
+
+          nativeBuildInputs = [ pkgs.pkg-config ];
+          PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+        };
+        in {
+        defaultPackage = wam;
+      devShell = pkgs.mkShell {
+        buildInputs = [
+          wam
+        ];
+      };
+      }
+      );
+      }
