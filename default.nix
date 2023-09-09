71e0ab33cae9c70cd6a4261c7023107313bae6ba
Waayway <thijs@vanwaaij.com>
2023-09-09 22:38:59 +0200

    fix: using buildRustPackage

--- default.nix
+++ default.nix
@@ -1,20 +1,32 @@
 {pkgs ? import <nixpkgs> {}, ...}: 
 
-pkgs.stdenv.mkDerivation {
-  name = "workspace-across-monitors";
+pkgs.rustPlatform.buildRustPackage rec {
+  pname = "wam";
+  version = "0.0.1";
+
   src = ./.;
 
-  nativeBuildInputs = with pkgs; [
-    rustc
-    cargo
-  ];
-
-  buildPhase = ''
-    cargo build --release
-  '';
-
-  installPhase = ''
-    mkdir -p $out/bin
-    cp target/release/wam $out/bin
-  '';
+  cargoLock = {
+    lockFile = ./Cargo.lock;
+  };
 }
+
+
+# pkgs.stdenv.mkDerivation {
+#   name = "workspace-across-monitors";
+#   src = ./.;
+
+#   nativeBuildInputs = with pkgs; [
+#     rustup
+#   ];
+
+#   buildPhase = ''
+#     rustup default stable
+#     cargo build --release
+#   '';
+
+#   installPhase = ''
+#     mkdir -p $out/bin
+#     cp target/release/wam $out/bin
+#   '';
+# }
